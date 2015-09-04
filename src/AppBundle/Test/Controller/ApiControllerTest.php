52b127792e75ad676343b49e1c4d384e3cf0a212
Antonio Prieto <antonio.prieto.riera@gmail.com>
2015-09-04 13:40:19 +0200

    Test añadidos

---
+++ src/AppBundle/Test/Controller/ApiControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+// src/AppBundle/Tests/Controller/ApiControllerTest.php
+namespace AppBundle/Tests/Controller;
+ 
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+ 
+class ApiControllerTest extends WebTestCase
+{
+	private function post($uri, array $data)
+	{
+		$content = json_encode($data);
+		$client = static::createClient();
+		$client->request('POST', $uri, array(), array(), array(), $content);
+
+		return $client->getResponse();
+	}
+
+    public function testInformacionSobreEmpresaExistente()
+    {
+        $response = $this->post('/api/informacion', array('empresa' => 'ACME'));
+  
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testInformacionSobreEmpresaInexistente()
+    {
+        $response = $this->post('/api/informacion', array('empresa' => 'NO_EXISTE'));
+ 
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+    }
+}
